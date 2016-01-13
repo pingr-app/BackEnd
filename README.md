28b7c89ce2425794224938cc72766286aae23c8b
Nima Boscarino <nboscari@sfu.ca>
2016-01-13 17:06:55 -0500

    Dummy API written in Sinatra, just for development purposes.

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # BackEnd: PROTOTYPE EDITION
 
-Here's an unordered list of methods we're planning on implementing that the front end guys can use with our API.
-
-Some of the info I'll write in the descriptions for the methods is kind of irrelevant to front end people, so this isn't like an actual document or anything, don't worry.
-
-This is literally the bare minimum. Okay, I know groups technically aren't the bare minimum but I really kind of want them in here right now so whatever.
-
-Note: every method needs to have that facebook authentication stuff passed along with it, so I'm not really going to write that down here because it's implied.
-
-- createUser (POST)
-  - Input: a UUID that's generated on the app itself, plus facebook stuff (for storage) I guess for now. First Name and last name.
-- deleteUser (DELETE)
-  - Input: UUID ...
-- createFriendship (POST)
-  - for now it'll be done with UUIDs, but we could do it instead with usernames which we'll force to be unique or emails
-  - Input: UUID, friend UUID, ..... to be completed.
-- deleteFriendship (DELETE)
-- createGroup (POST)
-- deleteGroup (DELETE)
-- getUser (GET)
-  - This is to find the info and location of any person. Wording here gets kind of tough...
-  - This takes as input: the app user's uuid and their authentication stuff, and the uuid of the user they are trying to get the info of. So first we check if the app user has an "accepted" friendship relation with the user they're looking for, and then if everything's all good we go to look up the current location of that user and package it up with that user's info and send it out.
-  - You should also be able to use this method on yourself.
+This is the documentation for the Pingr Backend API.
---
+++ dummy_api.rb
@@ -0,0 +1,96 @@
+# pingr.rb
+require 'sinatra'
+require 'json'
+require 'securerandom'
+require 'faker'
+
+get '/' do
+  status 200
+  JSON.pretty_generate({:response => "Welcome to the Pingr API :)!"})
+end
+
+get '/users' do
+  id = params['id']
+  return_message = {}
+  return_message[:id] = id 
+  return_message[:first_name] = Faker::Name.first_name
+  return_message[:last_name] = Faker::Name.last_name
+  return_message[:bio] = "A user"
+  status 200
+  JSON.pretty_generate(return_message)
+end
+
+post '/users' do
+  request.body.rewind
+  data = JSON.parse request.body.read
+
+  return_message = {}
+  return_message[:id] = SecureRandom.uuid
+  return_message[:first_name] = data['first_name']
+  return_message[:last_name] = data['last_name']
+  return_message[:bio] = data['bio']
+  status 201
+  JSON.pretty_generate(return_message)
+end
+
+get '/users/friends' do
+  id = params['id']
+  status = params['friend_status'] # status: confirmed, requested, pending
+  map_id = params['map_id'] # map_id: id for the map to filter friends by map (counts as loc=true)
+  loc = params['loc']       # loc   : true or false for location info
+
+
+  friends = []
+  
+  rand(10).times do
+    friends << {} 
+  end
+
+  friends.each do |friend|
+    friend[:id] = SecureRandom.uuid
+    friend[:first_name] = Faker::Name.first_name
+    friend[:last_name] = Faker::Name.last_name
+    if friend_status != "pending" && friend_status != "requested"
+      if loc == "true"
+        friend[:map_id] = rand(25)
+        friend[:x_coord] = rand(101)
+        friend[:y_coord] = rand(301)
+        friend[:time] = Faker::Time.between(DateTime.now - 1, DateTime.now)
+      end
+      if !map_id.nil?
+        friend[:map_id] = map_id
+      end
+    end
+  end
+  
+  status 200
+  JSON.pretty_generate({"id" => id, "friends" => friends})
+
+end
+
+post '/users/friends' do
+  # adding a friend
+  request.body.rewind
+  data = JSON.parse request.body.read
+  
+  return_message = {}
+  return_message[:id] = params["id"]
+  return_message[:friend_id] = data["friend_id"]
+  return_message[:first_name] = Faker::Name.first_name
+  return_message[:last_name] = Faker::Name.last_name
+  
+  i = rand(3)
+  if i == 0
+    friend_status = "confirmed"
+  elsif i == 1
+    friend_status = "pending"
+  elsif i == 2
+    friend_status = "requested"
+  end
+
+  return_message[:friend_status] = friend_status
+  status 201
+
+  JSON.pretty_generate(return_message)
+
+end
